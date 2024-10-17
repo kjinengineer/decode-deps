2208caa2af31a2c5a79333830b275d15bd717dc2
znzn <08monthmoon@naver.com>
2024-10-17 16:22:01 +0900

    feat: update fetch logic

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import { buildTree, extractNodesAndLinks, getDependencies } from "./getTree";
 import cors from "cors";
-import open from "open";
 
 const app = express();
 const port = 4000;
@@ -19,31 +18,11 @@ app.get("/track", (req, res) => {
 
   const dependencies = getDependencies(sourceDir);
   const dependencyTree = buildTree(dependencies, rootModule);
-
   const result = extractNodesAndLinks(dependencyTree);
 
-  res.send(`
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Module Analysis</title>
-        <script>
-          window.onload = function() {
-            const data = ${JSON.stringify(result)};
-            console.log(data);
-          };
-        </script>
-      </head>
-      <body>
-        <h1>hello world!</h1>
-      </body>
-    </html>
-  `);
+  res.json(result);
 });
 
 app.listen(port, () => {
   console.log("Server is running on http://localhost:4000");
-  open(`http://localhost:${port}/track`);
 });
