34440afb491a6a547289cdc69c741038d7ddf73e
zn <08monthmoon@naver.com>
2024-10-31 16:56:18 +0900

    feat: update cli

--- bin/cli.js
+++ bin/cli.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
-
 import startDepTrack from "../dist/route.js";
 
 const input = process.argv[2];
 
 if (!input) {
-  console.error("배열 형식의 입력값이 필요합니다!");
+  console.error("Input must be in ARRAY FORMAT!");
   process.exit(1);
 }
 
@@ -14,13 +13,13 @@ let inputArray;
 try {
   inputArray = JSON.parse(input);
   if (!Array.isArray(inputArray)) {
-    throw new Error("입력값이 배열 형식이 아닙니다!");
+    throw new Error("The input is not in ARRAY FORMAT!");
   }
 } catch (error) {
   console.error(
-    '배열 형식의 입력값을 JSON 형태로 전달해주세요. 예: \'["value1", "value2"]\''
+    'Please provide the input in JSON ARRAY FORMAT. Example: \'["value1", "value2"]\''
   );
   process.exit(1);
 }
 
-startDepTrack(["./src"]);
+startDepTrack(inputArray);
