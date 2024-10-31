0717aa34f39289b47f541aba40a0df99630e0f92
zn <08monthmoon@naver.com>
2024-10-31 15:09:26 +0900

    build: add cli setting

--- src/route.ts
+++ src/route.ts
@@ -3,21 +3,14 @@ import { fileURLToPath } from "url";
 import path, { dirname } from "path";
 import { buildTree, extractNodesAndLinks, getDependencies } from "./utils";
 
-interface InputProps {
-  sourceDir: string[];
-  rootModule: string;
-}
-
-export default function depTrack({ sourceDir, rootModule }: InputProps) {
+export default function startDepTrack(sourceDir: string[]) {
   const port = 5001;
-  const _filename = fileURLToPath(
-    require("url").pathToFileURL(__filename).toString()
-  );
+  const _filename = fileURLToPath(import.meta.url);
   const _dirname = dirname(_filename);
 
   const app = express();
-  app.use(express.static(path.join(_dirname, "./public")));
-  app.use(express.static(path.join(__dirname, "dist")));
+  app.use(express.static(path.join(_dirname, "../public")));
+  app.use(express.static(path.join(_dirname, "dist")));
 
   app.get("/", (req, res) => {
     res.sendFile(path.join(_dirname, "public", "index.html"));
@@ -25,7 +18,7 @@ export default function depTrack({ sourceDir, rootModule }: InputProps) {
 
   app.get("/track", (req, res) => {
     const dependencies = getDependencies(sourceDir);
-    const dependencyTree = buildTree(dependencies, rootModule);
+    const dependencyTree = buildTree(dependencies);
     const resultData = extractNodesAndLinks(dependencyTree);
 
     res.json(resultData);
